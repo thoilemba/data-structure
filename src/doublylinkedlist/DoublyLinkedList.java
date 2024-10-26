70492eac345020d4032a100e854e9733fdfa3d50
thoilemba <thoilembameetei.tm@gmail.com>
2024-10-26 13:32:35 +0530

    Modified DoublyLinkedList class

--- src/doublylinkedlist/DoublyLinkedList.java
+++ src/doublylinkedlist/DoublyLinkedList.java
@@ -23,7 +23,8 @@ public class DoublyLinkedList {
 	}
 	
 	// Printing the List
-	void printLinkedList() {
+	void printList() {
+		System.out.print("List: ");
 		Node current = head;
 		while(current != null) {
 			System.out.print(current.data +" ");
@@ -33,7 +34,8 @@ public class DoublyLinkedList {
 	}
 	
 	// Printing the List in reverse order
-	void printReverseLinkedList() {
+	void printListReverse() {
+		System.out.print("List: ");
 		Node current = tail;
 		while(current != null) {
 			System.out.print(current.data +" ");
@@ -44,6 +46,7 @@ public class DoublyLinkedList {
 	
 	// Adding a node at the end of the list
 	void append(int data) {
+		System.out.println("Append "+data+": ");
 		Node newNode = new Node(data);
 		
 		// for empty list
@@ -59,6 +62,7 @@ public class DoublyLinkedList {
 	
 	// Adding a node at the beginning of the list
 	void addFirst(int data) {
+		System.out.println("Add first "+data+": ");
 		Node newNode = new Node(data);
 		
 		// for empty list
@@ -72,9 +76,20 @@ public class DoublyLinkedList {
 		}
 	}
 	
+	// Finding the size of the List (same as singly linked list)
+	int size() {
+		int size = 0;
+		Node current = head;
+		while(current != null) {
+			current = current.next;
+			size++;
+		}
+		return size;
+	}
+	
 	// Adding a data at a specific index
-	void add(int data, int index) {
-		
+	void add(int index, int data) {
+		System.out.printf("Add %d at %d: ", data, index);
 		if(index < 0 ||index > size()) {
 			throw new IndexOutOfBoundsException("Index out of bound for index "+index);
 		}
@@ -98,7 +113,49 @@ public class DoublyLinkedList {
 			current.next = newNode;
 		}
 	}
-	
+
+	// Adding a data at a specific index without using other methods
+	void add2(int index, int data) throws IndexOutOfBoundsException{
+		System.out.printf("Add %d at index %d: \n", data, index);
+		if(index < 0) {
+			throw new IndexOutOfBoundsException("Negative index provided.");
+		}
+		
+		Node newNode = new Node(data);
+		if(index == 0) {
+			if(head == null) {
+				head = newNode;
+				return;
+			}
+			
+			head.previous = newNode;
+			newNode.next = head;
+			head = newNode;
+			return;
+		}
+		
+		int count = 0;
+		Node current = head;
+		while(count != index-1) {
+			if(current == null) {
+				throw new IndexOutOfBoundsException("Index out of bound for index "+index);
+			}
+			current = current.next;
+			count++;
+		}
+		
+		if(current == null) {
+			throw new IndexOutOfBoundsException("Index out of bound for index "+index);
+		}
+		
+		newNode.next = current.next;
+		if(current.next != null) {
+			current.next.previous = newNode;
+		}
+		
+		current.next = newNode;
+	}
+
 	// Adding an array of nodes to the end of the List
 	void addAll(int[] elements) {
 		for(int element : elements) {
@@ -108,6 +165,7 @@ public class DoublyLinkedList {
 	
 	// Removing first node from the List
 	void removeFirst() {
+		System.out.print("Remove first: ");
 		if(head == null) {
 			System.out.println("Already an empty list.");
 			return;
@@ -125,6 +183,7 @@ public class DoublyLinkedList {
 	
 	// Removing first node from the List
 	void removeLast() {
+		System.out.print("Remove last: ");
 		if(head == null) {
 			System.out.println("Already an empty list.");
 			return;
@@ -139,46 +198,69 @@ public class DoublyLinkedList {
 			tail.next = null;
 		}
 	}
-		
+
 	// Removing a node from a specific index
-	void remove(int index) {
-		if(head == null || index < 0) {
+	void remove(int index) throws IndexOutOfBoundsException{
+		System.out.println("Remove from index "+index+": ");
+		if(index < 0) {
 			throw new IndexOutOfBoundsException();
 		}
 		
-		// having single element
-		if(head.next == null && index == 0) {
+		 // single element
+		if(index == 0 && head == tail) {
 			head = null;
 			tail = null;
-			return;
-		}
-		
-    	// removing the first index
-    	if(index == 0) {
-    		head = head.next;
-    		head.previous = null;
-    		return;
-    	}
-
-		try {
+		}else if(index == 0) {
+			head = head.next;
+			head.previous = null;
+		}else {
 			Node current = head;
 			int count = 0;
-			// traversing to the previous element of the specified index
 			while(count < index-1) {
+				if(current == null) {
+					throw new IndexOutOfBoundsException();
+				}
 				current = current.next;
 				count++;
 			}
+			if(current == null) {
+				throw new IndexOutOfBoundsException();
+			}
+			
 			if(current.next.next == null) {
 				current.next = null;
-				tail = null;
+				tail = current;
 			}else {
 				current.next = current.next.next;
-				tail.previous = current;
-				tail = current.next;
 			}
-		}catch(Exception e) {
-			throw new IndexOutOfBoundsException();
 		}
+	}
+	
+	// Deleting a specified node from the List
+	void deleteNode(Node nodeToDelete) {
+		if(nodeToDelete == null) {
+			return;
+		}
+		
+		// Adjust the next node's previous reference
+		if(nodeToDelete.next != null) {
+			nodeToDelete.next.previous = nodeToDelete.previous;
+		}else {
+			// If the node to delete is the tail, update the tail
+			tail = nodeToDelete.previous;
+		}
+		
+		// Adjust the previous node's next reference
+		if(nodeToDelete.previous != null) {
+			nodeToDelete.previous.next = nodeToDelete.next;
+		}else {
+			// If the node to delete is the head, update the head
+			head = nodeToDelete.next;
+		}
+		
+		 // Clean up the node's references
+		nodeToDelete.previous = null;
+		nodeToDelete.next = null;
 	}
 
 	// Finding the index of an element (same as singly linked list)
@@ -212,104 +294,57 @@ public class DoublyLinkedList {
 		
 		return result;
 	}
-	
-	// Finding the size of the List (same as singly linked list)
-	int size() {
-		int size = 0;
-		Node current = head;
-		while(current != null) {
-			current = current.next;
-			size++;
-		}
-		return size;
-	}
-	
-	// Deleting a specified node from the List
-	void deleteNode(Node nodeToDelete) {
-		if(nodeToDelete == null) {
-			return;
-		}
-		
-		// Adjust the next node's previous reference
-		if(nodeToDelete.next != null) {
-			nodeToDelete.next.previous = nodeToDelete.previous;
-		}else {
-			// If the node to delete is the tail, update the tail
-			tail = nodeToDelete.previous;
-		}
-		
-		// Adjust the previous node's next reference
-		if(nodeToDelete.previous != null) {
-			nodeToDelete.previous.next = nodeToDelete.next;
-		}else {
-			// If the node to delete is the head, update the head
-			head = nodeToDelete.next;
-		}
-		
-		 // Clean up the node's references
-		nodeToDelete.previous = null;
-		nodeToDelete.next = null;
-	}
+
 
 	public static void main(String[] args) {
+		DoublyLinkedList list = new DoublyLinkedList();
+		Node node1 = new Node(30);
+		Node node2 = new Node(70);
+		list.head = node1;
+		list.tail = node1;
+		list.head.next = node2;
+		list.tail = node2;
+		list.tail.previous = list.head;
+		System.out.println("head: "+ list.head.data);
+		System.out.println("head.next: "+ list.head.next.data);
+		System.out.println("tail: "+ list.tail.data);
+		System.out.println("tail.previous: "+ list.tail.previous.data);
+		list.printList();
+		list.printListReverse();
 		
-		DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
-		// Comment all these lines below
-		// Then start un-commenting line by line and run to check each output
-		
-		doublyLinkedList.append(10);
-		doublyLinkedList.append(20);
-		doublyLinkedList.append(30);
-		doublyLinkedList.append(40);
-		doublyLinkedList.append(50);
-		System.out.print("Adding element using append() : ");
-		doublyLinkedList.printLinkedList();
-		System.out.println("head : "+ doublyLinkedList.head.data);
-		System.out.println("tail : "+ doublyLinkedList.tail.data);
-		
-		System.out.print("Printing the list in reverse order : ");
-		doublyLinkedList.printReverseLinkedList();
-		
-		doublyLinkedList.addFirst(40);
-		System.out.print("Adding element using addFirst() : ");
-		doublyLinkedList.printLinkedList();
-		
-		doublyLinkedList.add(25, 3);
-		System.out.print("Adding element using add(index) : ");
-		doublyLinkedList.printLinkedList();
-		
-		int[] arr = {40, 50};
-		doublyLinkedList.addAll(arr);
-		System.out.print("Adding element using addAll() : ");
-		doublyLinkedList.printLinkedList();
-		
-		System.out.print("Removing element using removeFirst() : ");
-		doublyLinkedList.removeFirst();
-		doublyLinkedList.printLinkedList();
-		
-		System.out.print("Removing element using removeLast() : ");
-		doublyLinkedList.removeLast();
-		doublyLinkedList.printLinkedList();
-		
-		System.out.print("Removing element using remove(index) : ");
-		doublyLinkedList.remove(2);
-		doublyLinkedList.printLinkedList();
-		
-		int result = doublyLinkedList.indexOf(40);
-		System.out.println("Index of 40 : "+ result);
-		int result2 = doublyLinkedList.lastIndexOf(40);
-		System.out.println("Last index of 40 : "+ result2);
-		int result3 = doublyLinkedList.indexOf(70);
-		System.out.println("Index of 70 : "+ result3);
-		int result4 = doublyLinkedList.lastIndexOf(70);
-		System.out.println("Last index of 70 : "+ result4);
-		
-		System.out.println("Size of the LinkedList : "+ doublyLinkedList.size());
-		
-		System.out.print("Deleting a node using delete(node) : ");
-		doublyLinkedList.deleteNode(doublyLinkedList.head.next.next.next);
-		doublyLinkedList.printLinkedList();
+		// INSERTION
+		System.out.println("INSERTION:");
+		list.append(40);
+		list.append(10);
+		list.printList();
+		list.addFirst(60);
+		list.addFirst(20);
+		list.printList();
+		list.add(6, 50);
+		list.printList();
+		list.add2(7, 80);
+		list.printList();
+		int[] arr = {2, 3, 6};
+		list.addAll(arr);
+		list.printList();
+		//	DELETION
+		System.out.println("DELETION:");
+		list.removeFirst();
+		list.printList();
+		list.removeLast();
+		list.printList();
+		list.remove(7);
+		list.printList();
+		list.printListReverse();
+		list.deleteNode(list.tail.previous);
+		list.printList();
+		// INDEX OF
+		System.out.println("INDEX OF:");
+		System.out.println("Index of 2: "+ list.indexOf(2));
+		list.addFirst(2);
+		list.printList();
+		System.out.println("Last index of 2: "+ list.lastIndexOf(2));
+		System.out.println("Size: "+list.size());
 
 	}
-
 }
